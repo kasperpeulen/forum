875c5b3a47bfd195b34e0bd2bbf8233b6c86fa9c
Kasper Peulen <kasperpeulen@gmail.com>
2017-03-24 16:05:04 +0100

    feat: a_user_can_browse_threads

--- tests/Feature/ThreadsTest.php
+++ tests/Feature/ThreadsTest.php
@@ -7,17 +7,16 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ExampleTest extends TestCase
+class ThreadsTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testBasicTest()
-    {
-        $response = $this->get('/');
+    use DatabaseMigrations;
 
+    /**
+     * @test
+     */
+    public function a_user_can_browse_threads()
+    {
+        $response = $this->get('/threads');
         $response->assertStatus(200);
     }
 }
