aaf5a415542c3972cc3fa653371c9e0150562395
Kasper Peulen <kasperpeulen@gmail.com>
2017-03-25 18:53:29 +0100

    feat: nav for create thread and fix default route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -43,7 +43,8 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        <li><a href="/threads">Threads</a></li>
+                        <li><a href="/threads">All threads</a></li>
+                        <li><a href="/threads/create">Create thread</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
