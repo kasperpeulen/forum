169d9d9838d7e28e1c688996ffe1c0ff64b1770a
Kasper Peulen <kasperpeulen@gmail.com>
2017-03-29 13:46:12 +0200

    feat: change error message

--- resources/views/layouts/errors.blade.php
+++ resources/views/layouts/errors.blade.php
@@ -1,7 +1,7 @@
 @if(count($errors) > 0)
     <div class="form-group">
         <div class="alert alert-danger alert-important">
-            <strong>Errors:</strong>
+            <strong>Whoops! There were some problems with your input.</strong>
             <ul>
                 @foreach($errors->all() as $error)
                     <li>{{$error}}</li>
